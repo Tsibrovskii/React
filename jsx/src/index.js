61707174f7e1f9dc0c30e71a7390d88054e92991
Aleksei <tsibrovskii@gmail.com>
2020-02-08 18:48:53 +0300

    jsx: usage of variables

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -2,7 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  return <div>Hi there!</div>
+  const buttonText = 'Click me!';
+  const labelText = 'Enter name:';
+
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        {labelText}
+      </label>
+      <input id="name" type="text" />
+      <button style={{backgroundColor: 'blue', color: 'white'}}>
+        {buttonText}
+      </button>
+    </div>
+  )
 }
 
 ReactDOM.render(
