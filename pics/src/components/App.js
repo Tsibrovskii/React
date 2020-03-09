ae09058b651a64ecca97833221a898a7ff75d968
Aleksei <tsibrovskii@gmail.com>
2020-03-09 21:52:55 +0300

    pics: added axios

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term);
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: {query: term},
+      headers: {
+        Authorization: 'Client-ID ddY7q4CtsGaYuCoE7CCCnxeT3dZhVLxdkvjzm_dtc_c'
+      }
+    });
   }
   render() {
     return (
