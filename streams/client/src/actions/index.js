bb4b3e41c267872010361c10dc17b51876952c3b
Aleksei Tsibrovskii <alekseitsibrovskii@Alekseis-MacBook-Pro.local>
2020-07-29 02:09:56 +0300

    edit stream implementation

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -37,16 +37,17 @@ export const fetchStreams = () => async dispatch => {
   dispatch({ type: FETCH_STREAMS, payload: response.data });
 };
 
-export const fetchStream = (id) => async dispatch => {
+export const fetchStream = id => async dispatch => {
   const response = await streams.get(`/streams/${id}`);
 
   dispatch({ type: FETCH_STREAM, payload: response.data });
 };
 
 export const editStream = (id, formValues) => async dispatch => {
-  const response = await streams.put(`/streams/${id}`, formValues);
+  const response = await streams.patch(`/streams/${id}`, formValues);
 
   dispatch({ type: EDIT_STREAM, payload: response.data });
+  history.push('/');
 };
 
 export const deleteStream = (id) => async dispatch => {
