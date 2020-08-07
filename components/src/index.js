18ab8fb9386adcd6d88f584a27e2ca0857d04192
Aleksei Tsibrovskii <alekseitsibrovskii@Alekseis-MacBook-Pro.local>
2020-08-08 02:56:45 +0300

    fix name

--- components/src/index.js
+++ components/src/index.js
@@ -2,41 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
-import ApprovaCard from './ApprovalCard';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <ApprovaCard>
+      <ApprovalCard>
         <div>
           <h1>Warning!</h1>
           Are you sure?
         </div>
-      </ApprovaCard>
-      <ApprovaCard>
+      </ApprovalCard>
+      <ApprovalCard>
         <CommentDetail
           author="Sam"
           timeAgo="Today at 4:45PM"
           content="Nice blog post!"
           avatar={faker.image.avatar()}
         />
-      </ApprovaCard>
-      <ApprovaCard>
+      </ApprovalCard>
+      <ApprovalCard>
         <CommentDetail
           author="Alex"
           timeAgo="Today at 2:00AM"
           content="Agree with you!"
           avatar={faker.image.avatar()}
         />
-      </ApprovaCard>
-      <ApprovaCard>
+      </ApprovalCard>
+      <ApprovalCard>
         <CommentDetail
           author="Jane"
           timeAgo="Yesterday at 5:00PM"
           content="Hi there!"
           avatar={faker.image.avatar()}
         />
-      </ApprovaCard>
+      </ApprovalCard>
     </div>
   )
 }
