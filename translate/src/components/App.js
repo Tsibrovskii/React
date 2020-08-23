f8633946a2faf9f3a066f86559e9af340a052771
Aleksei Tsibrovskii <alekseitsibrovskii@Alekseis-MacBook-Pro.local>
2020-08-23 12:01:58 +0300

    translate: replace redux by context

--- translate/src/components/App.js
+++ translate/src/components/App.js
@@ -1,39 +1,30 @@
 import React from 'react';
 import UserCreate from './UserCreate';
-import LanguageContext from '../contexts/LanguageContext';
+import { LanguageStore } from '../contexts/LanguageContext';
 import ColorContext from '../contexts/ColorContext';
+import LanguageSelector from './LanguageSelector';
 
 class App extends React.Component {
   state = { language: 'english', color: 'red' };
 
-  onLanguageChange = language => {
-    this.setState({ language });
-    if (language === 'english') {
-      this.setState({ color: 'red' });
-    } else {
-      this.setState({ color: 'green' });
-    }
-  };
+  // onLanguageChange = language => {
+  //   this.setState({ language });
+  //   if (language === 'english') {
+  //     this.setState({ color: 'red' });
+  //   } else {
+  //     this.setState({ color: 'green' });
+  //   }
+  // };
 
   render() {
     return (
       <div className="ui container">
-        <div>
-          Select a language:
-          <i
-            className="flag us"
-            onClick={() => this.onLanguageChange('english')}
-          />
-          <i
-            className="flag nl"
-            onClick={() => this.onLanguageChange('dutch')}
-          />
-        </div>
-        <ColorContext.Provider value={this.state.color}>
-          <LanguageContext.Provider value={this.state.language}>
+        <LanguageStore>
+          <LanguageSelector />
+          <ColorContext.Provider value={this.state.color}>
             <UserCreate />
-          </LanguageContext.Provider>
-        </ColorContext.Provider>
+          </ColorContext.Provider>
+        </LanguageStore>
       </div>
     );
   }
